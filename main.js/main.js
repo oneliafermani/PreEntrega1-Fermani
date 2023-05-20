7245147b5a6fbab25badadcd203a5bca8b9b7186
oneliafermani <fermanionelia5@gmail.com>
2023-05-20 11:41:02 -0300

    agrego segunda entrega

--- main.js/main.js
+++ main.js/main.js
@@ -1,39 +1,51 @@
-let cantidadAlumnos = 0;
-let notasEvaluaciones = [];
-let notaFinalAlumnos = [];
 
-function calcularNotaFinal(nota1, nota2, nota3, nota4) {
-  let notaFinal = (nota1 * 0.2) + (nota2 * 0.3) + (nota3 * 0.3) + (nota4 * 0.2);
-  return notaFinal;
+let cantidadAlumnos = 0;
+let alumnos = [];
+
+function Alumno(nombre) {
+  this.nombre = nombre;
+  this.notas = [];
+  this.notaFinal = 0;
+}
+
+// Método para calcular la nota final del alumno
+Alumno.prototype.calcularNotaFinal = function() {
+  let notaFinal = (this.notas[0] * 0.2) + (this.notas[1] * 0.3) + (this.notas[2] * 0.3) + (this.notas[3] * 0.2);
+  this.notaFinal = notaFinal;
 };
 
 cantidadAlumnos = prompt("Ingrese la cantidad de alumnos:");
 cantidadAlumnos = parseInt(cantidadAlumnos);
 
 for (let i = 0; i < cantidadAlumnos; i++) {
-  let notas = [];
+  let nombre = prompt("Ingrese el nombre del alumno " + (i + 1) + ":");
+  let alumno = new Alumno(nombre);
+
   for (let j = 1; j <= 4; j++) {
     let nota = 0;
     while (nota < 1 || nota > 10) {
-      nota = prompt("Ingrese la nota de la evaluación " + j + " del alumno " + (i + 1) + ":");
+      nota = prompt("Ingrese la nota de la evaluación " + j + " del alumno " + alumno.nombre + ":");
       nota = parseFloat(nota);
       if (nota < 1 || nota > 10) {
         alert("La nota ingresada es inválida. Ingrese una nota entre 1 y 10.");
-      };
-    };
-    notas.push(nota);
-  };
-  notasEvaluaciones.push(notas);
-};
+      }
+    }
+    alumno.notas.push(nota);
+  }
 
-for (let i = 0; i < cantidadAlumnos; i++) {
-  let notas = notasEvaluaciones[i];
-  let notaFinal = calcularNotaFinal(notas[0], notas[1], notas[2], notas[3]);
-  notaFinalAlumnos.push(notaFinal);
-};
+  alumno.calcularNotaFinal();
+  alumnos.push(alumno);
+}
 
-for (let i = 0; i < cantidadAlumnos; i++) {
-  let notaFinal = notaFinalAlumnos[i];
-  alert("La nota final del alumno " + (i + 1) + " es: " + notaFinal);
-};
+// Búsqueda de un alumno por nombre
+let nombreBuscado = prompt("Ingrese el nombre del alumno que desea buscar:");
+let alumnoEncontrado = alumnos.find(alumno => alumno.nombre === nombreBuscado);
+if (alumnoEncontrado) {
+  console.log("Alumno encontrado:", alumnoEncontrado);
+} else {
+  console.log("Alumno no encontrado.");
+}
 
+// Filtrado de alumnos con nota final mayor o igual a 7
+let alumnosAprobados = alumnos.filter(alumno => alumno.notaFinal >= 7);
+console.log("Alumnos aprobados:", alumnosAprobados);
